373f0700783f54498eb407823e91e83a154e9a6d
Duane David <duanedave17@gmail.com>
2022-11-03 07:21:13 -0700

    Add style to hero section.js

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,19 +1,29 @@
 import styles from './HeroSection.module.css';
+import Socials from '../../reusables/socials/Socials';
 
 function HeroSection() {
   return (
     <section className={styles.heroContainer}>
       <div className={styles.leftHalf}>
-        <h1>Software Developer</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde nobis
-          quam mollitia maxime, dicta iusto dignissimos exercitationem autem
-          nostrum expedita?
-        </p>
+        <div>
+          <h1>
+            Software
+            <br />
+            Developer.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde nobis
+            quam mollitia maxime, dicta iusto dignissimos exercitationem autem
+            nostrum expedita?
+          </p>
+        </div>
       </div>
       <div className={styles.rightHalf}>
         <div className={styles.placeholder}></div>
       </div>
+      <span className={styles.socialLinks}>
+        <Socials className={styles.socialIcon} />
+      </span>
     </section>
   );
 }
