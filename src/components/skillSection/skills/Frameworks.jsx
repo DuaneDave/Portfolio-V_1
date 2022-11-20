fef1d9c5c1626e348c84fb7c338f73c876c8f255
Duane David <duanedave17@gmail.com>
2022-11-20 06:04:07 -0800

    Add component to render framework

---
+++ src/components/skillSection/skills/Frameworks.jsx
@@ -0,0 +1,12 @@
+import styles from '../Skills.module.css';
+import Framework from './tags/Framework';
+
+function Frameworks() {
+  return (
+    <div>
+      <Framework />
+    </div>
+  );
+}
+
+export default Frameworks;
