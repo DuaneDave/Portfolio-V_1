f44992810ff9c5c50d092ef083465baff713e5d5
Duane David <duanedave17@gmail.com>
2022-11-20 06:06:01 -0800

    render skill component

--- src/components/skillSection/Skills.jsx
+++ src/components/skillSection/Skills.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import FrontEnd from './skills/FrontEnd';
-import BackEnd from './skills/BackEnd';
+import Container from '../../reusables/container/Container';
+import Languages from './skills/Languages';
+import Frameworks from './skills/Frameworks';
 import Professional from './skills/Professional';
+import Tools from './skills/Tools';
 import styles from './Skills.module.css';
 
 function Skills() {
   const [isTabbed, setIsTabbed] = useState(0);
   const keyPressHandler = (e) => {
     if (e.key === 'ArrowRight') {
-      if (isTabbed === 2) {
+      if (isTabbed === 3) {
         setIsTabbed(0);
       } else {
         setIsTabbed((prev) => prev + 1);
@@ -19,7 +21,7 @@ function Skills() {
 
     if (e.key === 'ArrowLeft') {
       if (isTabbed === 0) {
-        setIsTabbed(2);
+        setIsTabbed(3);
       } else {
         setIsTabbed((prev) => prev - 1);
       }
@@ -27,68 +29,94 @@ function Skills() {
   };
 
   return (
-    <div>
-      <ul className={styles.tabHeader}>
-        <li
-          tabIndex="0"
-          onClick={() => setIsTabbed(0)}
-          onFocus={() => setIsTabbed(0)}
-          onKeyDown={keyPressHandler}
-        >
-          Front End
-          {isTabbed === 0 && (
-            <motion.span
-              layoutId="underline"
-              className={styles.active}
-            ></motion.span>
-          )}
-        </li>
+    <Container className={styles.skills}>
+      <div className={styles.skillDetails}>
+        <h2>My skills.</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          provident ipsum eum eaque minima tenetur distinctio rem sint maxime
+          facere jhbjk knml.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <ul className={styles.tabHeader}>
+          <li
+            tabIndex="0"
+            onClick={() => setIsTabbed(0)}
+            onFocus={() => setIsTabbed(0)}
+            onKeyDown={keyPressHandler}
+          >
+            Languages
+            {isTabbed === 0 && (
+              <motion.span
+                layoutId="underline"
+                className={styles.active}
+              ></motion.span>
+            )}
+          </li>
 
-        <li
-          tabIndex="1"
-          onClick={() => setIsTabbed(1)}
-          onFocus={() => setIsTabbed(1)}
-          onKeyDown={keyPressHandler}
-        >
-          Back End
-          {isTabbed === 1 && (
-            <motion.span
-              layoutId="underline"
-              className={styles.active}
-            ></motion.span>
-          )}
-        </li>
+          <li
+            tabIndex="1"
+            onClick={() => setIsTabbed(1)}
+            onFocus={() => setIsTabbed(1)}
+            onKeyDown={keyPressHandler}
+          >
+            Frameworks
+            {isTabbed === 1 && (
+              <motion.span
+                layoutId="underline"
+                className={styles.active}
+              ></motion.span>
+            )}
+          </li>
 
-        <li
-          tabIndex="2"
-          onClick={() => setIsTabbed(2)}
-          onFocus={() => setIsTabbed(2)}
-          onKeyDown={keyPressHandler}
-        >
-          Professional
-          {isTabbed === 2 && (
-            <motion.span
-              layoutId="underline"
-              className={styles.active}
-            ></motion.span>
-          )}
-        </li>
-      </ul>
+          <li
+            tabIndex="2"
+            onClick={() => setIsTabbed(2)}
+            onFocus={() => setIsTabbed(2)}
+            onKeyDown={keyPressHandler}
+          >
+            Tools
+            {isTabbed === 2 && (
+              <motion.span
+                layoutId="underline"
+                className={styles.active}
+              ></motion.span>
+            )}
+          </li>
 
-      <AnimatePresence exitBeforeEnter>
-        <motion.div
-          key={isTabbed}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ y: -10, opacity: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          {isTabbed === 0 && <FrontEnd />}
-          {isTabbed === 1 && <BackEnd />}
-          {isTabbed === 2 && <Professional />}
-        </motion.div>
-      </AnimatePresence>
-    </div>
+          <li
+            tabIndex="3"
+            onClick={() => setIsTabbed(3)}
+            onFocus={() => setIsTabbed(3)}
+            onKeyDown={keyPressHandler}
+          >
+            Professional
+            {isTabbed === 3 && (
+              <motion.span
+                layoutId="underline"
+                className={styles.active}
+              ></motion.span>
+            )}
+          </li>
+        </ul>
+
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={isTabbed}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            {isTabbed === 0 && <Languages />}
+            {isTabbed === 1 && <Frameworks />}
+            {isTabbed === 2 && <Tools />}
+            {isTabbed === 3 && <Professional />}
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </Container>
   );
 }
 
