9c1e90b8767426c4f2a009ceb2bbae22f1e3951d
Duane David <duanedave17@gmail.com>
2022-11-20 06:03:48 -0800

    Add component to render language

---
+++ src/components/skillSection/skills/Languages.jsx
@@ -0,0 +1,12 @@
+import styles from '../Skills.module.css';
+import Language from './tags/Language';
+
+function Languages() {
+  return (
+    <div>
+      <Language />
+    </div>
+  );
+}
+
+export default Languages;
