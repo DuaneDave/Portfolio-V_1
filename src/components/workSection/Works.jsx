a9f65863456889bc6cdfbca676e973af61e356f8
Duane David <duanedave17@gmail.com>
2022-12-17 13:11:04 -0800

    Add and style the work component

--- src/components/workSection/Works.jsx
+++ src/components/workSection/Works.jsx
@@ -1,8 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { BsArrowRight } from 'react-icons/bs';
+
+import Container from '../../reusables/container/Container';
+import styles from './Works.module.css';
+
 function Works() {
   return (
-    <div>
-      <h1>Works</h1>
-    </div>
+    <Container className={styles.works}>
+      <div className={styles.placeholder}>
+        <div className={styles.projects}>
+          <h3>
+            A catalog <br /> of my works.
+          </h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+          <a href="#" className={styles.worksButton}>
+            View All
+            <BsArrowRight className={styles.icon} />
+          </a>
+        </div>
+
+        <div className={styles.article}>
+          <h3>
+            A catalog <br /> of my articles.
+          </h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+          <a href="#" className={styles.worksButton}>
+            View All
+            <BsArrowRight className={styles.icon} />
+          </a>
+        </div>
+      </div>
+    </Container>
   );
 }
 
