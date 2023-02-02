847e5070a970977ae5624c86bb6d61724dfa29e3
Duane David <duanedave17@gmail.com>
2023-02-02 20:56:36 +0100

    render project details element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { Suspense, Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './index.css';
 
 import Navigation from './components/navigation/Navigation';
-import HeroSection from './components/heroSection/HeroSection';
-import AboutSection from './components/aboutSection/AboutSection';
-import Skills from './components/skillSection/Skills';
-import Works from './components/workSection/Works';
-import Contact from './components/contactSection/Contact';
+import Home from './pages/Home';
+import Articles from './components/workSection/Articles';
+import Projects from './components/workSection/Projects';
+import ProjectDetailsPage from './pages/ProjectDetailsPage';
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Fragment>
         <Navigation />
-        <HeroSection />
-        <AboutSection />
-        <Skills />
-        <Works />
-        <Contact />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
+        </Routes>
       </Fragment>
     </Suspense>
   );
