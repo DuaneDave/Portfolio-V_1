d32d7d9b2cd564b40332e03e355fb46dfd2be227
Duane David <duanedave17@gmail.com>
2023-02-02 20:55:23 +0100

    Add a project details page

---
+++ src/components/workSection/ProjectDetails.jsx
@@ -0,0 +1,36 @@
+import Carousel from './components/Carousel';
+import styles from './ProjectDetails.module.css';
+import { AiFillGithub } from 'react-icons/ai';
+import { BsArrowUpRight } from 'react-icons/bs';
+
+function ProjectDetails({ payload }) {
+  const { title, description, tech_stack, live_link, github_link } = payload;
+
+  return (
+    <section className={styles.detailsContainer + ' flex align-x'}>
+      <div className={styles.details + ' flex flex-column align-y'}>
+        <h2>{title}</h2>
+        <div className={styles.links + ' flex'}>
+          <a href={live_link} className="flex align-y">
+            Live demo
+            <BsArrowUpRight />
+          </a>
+          <a href={github_link} className="flex align-y">
+            View source
+            <AiFillGithub />
+          </a>
+        </div>
+        <Carousel images={payload.screenshots} />
+
+        <div className={styles.stack + ' flex align-x'}>
+          {tech_stack.map((stack, index) => (
+            <span key={index}>{stack}</span>
+          ))}
+        </div>
+        <p>{description}</p>
+      </div>
+    </section>
+  );
+}
+
+export default ProjectDetails;
