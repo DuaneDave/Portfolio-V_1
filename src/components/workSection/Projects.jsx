e73d6f54f49decc09ba59a47f2666f391b29adf5
Duane David <duanedave17@gmail.com>
2023-02-02 20:55:04 +0100

    Add a project componnent and route

--- src/components/workSection/Projects.jsx
+++ src/components/workSection/Projects.jsx
@@ -1,10 +1,41 @@
 import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
+import projects from '../../data/projects';
+
+import styles from './Projects.module.css';
 
 function Projects() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <section className={styles.container}>
+      <span>
+        <h2>./projects</h2>
+        <p>Selected projects i've worked on in the past</p>
+      </span>
+      <div className={styles.cardContainer}>
+        {projects.map((project) => (
+          <motion.div
+            key={project.id}
+            className={styles.card}
+            whileHover={{
+              translateY: -8,
+            }}
+            transition={{
+              type: 'spring',
+              stiffness: 300,
+            }}
+            onClick={() => navigate(`/projects/${project.id}`)}
+          >
+            <img src={project.screenshots[0]} alt="project screenshot" />
+
+            <span>
+              <p>project name</p>
+              <a href={project.live_link}>see project</a>
+            </span>
+          </motion.div>
+        ))}
+      </div>
+    </section>
   );
 }
 
